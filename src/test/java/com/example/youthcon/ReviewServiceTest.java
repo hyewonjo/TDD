3b7d3b882631071a95c0a954abddfae80f3f365a
hyewonjo <hyewonjo92@gmail.com>
2021-12-29 15:48:24 +0900

    ReviewServiceTest - magic number -> fixture 로 수정

--- src/test/java/com/example/youthcon/ReviewServiceTest.java
+++ src/test/java/com/example/youthcon/ReviewServiceTest.java
@@ -13,16 +13,22 @@ class ReviewServiceTest {
     ReviewRepository reviewRepository = mock(ReviewRepository.class);
     ReviewService reviewService = new ReviewService(reviewRepository);
 
+    private Long id = 1L;
+    private String content = "재밌어요";
+    private String phoneNumber = "010-1111-2222";
+
     @Test
     void 후기_조회_성공() {
         // 준비
-        given(reviewRepository.findById(1L)).willReturn(Optional.of(new Review(1L, "재밌어요", "010-1111-2222")));
+        given(reviewRepository.findById(id)).willReturn(Optional.of(new Review(id, content, phoneNumber)));
 
         // 실행
-        Review review = reviewService.getById(1L);
+        Review review = reviewService.getById(id);
 
         // 검증
-        assertThat(review.getId()).isEqualTo(1);
+        assertThat(review.getId()).isEqualTo(id);
+        assertThat(review.getContent()).isEqualTo(content);
+        assertThat(review.getPhoneNumber()).isEqualTo(phoneNumber);
     }
 
 }
