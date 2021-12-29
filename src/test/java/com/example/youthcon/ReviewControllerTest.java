b3f5eff102419441552d6ed6f3f3a79411a589e2
hyewonjo <hyewonjo92@gmail.com>
2021-12-29 09:19:37 +0900

    Controller Test 작성

---
+++ src/test/java/com/example/youthcon/ReviewControllerTest.java
@@ -0,0 +1,28 @@
+package com.example.youthcon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+public class ReviewControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void 후기_조회_성공() throws Exception {
+        // 준비
+
+        // 실행
+        ResultActions perform = mockMvc.perform(get("/reviews/1"));
+
+        // 검증
+        perform.andExpect(status().isOk());
+    }
+}
