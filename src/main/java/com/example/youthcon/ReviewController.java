d49c22db81ad08a8eaf239df24a9338cf73c4bf5
hyewonjo <hyewonjo92@gmail.com>
2021-12-29 16:14:03 +0900

    ReviewController에 ReviewService 주입 추가

--- src/main/java/com/example/youthcon/ReviewController.java
+++ src/main/java/com/example/youthcon/ReviewController.java
@@ -6,8 +6,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ReviewController {
+
+    private final ReviewService reviewService;
+
+    public ReviewController(ReviewService reviewService) {
+        this.reviewService = reviewService;
+    }
+
     @GetMapping("/reviews/{id}")
-    public Object getReview(@PathVariable Long id) {
-        return null;
+    public Review getReview(@PathVariable Long id) {
+        return reviewService.getById(id);
     }
 }
