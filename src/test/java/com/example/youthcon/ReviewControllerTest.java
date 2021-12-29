f1e5457ef13e1d5e0c79bcb54ab8199c83a092f6
hyewonjo <hyewonjo92@gmail.com>
2021-12-29 16:17:17 +0900

    ReviewControllerTest에 MockBean으로 ReviewService bean 생성, given 작성

--- src/test/java/com/example/youthcon/ReviewControllerTest.java
+++ src/test/java/com/example/youthcon/ReviewControllerTest.java
@@ -1,8 +1,10 @@
 package com.example.youthcon;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
@@ -16,19 +18,26 @@ public class ReviewControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    @MockBean
     private ReviewService reviewService;
+
+    private Long id = 1L;
+    private String content = "재밌어요";
+    private String phoneNumber = "010-1111-2222";
 
     @Test
     void 후기_조회_성공() throws Exception {
         // 준비
+        BDDMockito.given(reviewService.getById(id))
+                .willReturn(new Review(id, content, phoneNumber));
 
         // 실행
-        ResultActions perform = mockMvc.perform(get("/reviews/1"));
+        ResultActions perform = mockMvc.perform(get("/reviews/" + id));
 
         // 검증
         perform.andExpect(status().isOk())
-                .andExpect(jsonPath("id").value(1))
-                .andExpect(jsonPath("content").value("재밌어요"))
-                .andExpect(jsonPath("phoneNumber").value("010-1111-2222"));
+                .andExpect(jsonPath("id").value(id))
+                .andExpect(jsonPath("content").value(content))
+                .andExpect(jsonPath("phoneNumber").value(phoneNumber));
     }
 }
